ad40678905e3974e2a83e66d652e33e3d46de5e1
Hai Nguyen <hainm.comp@gmail.com>
2016-02-22 00:53:43 -0500

    add install

--- devtools/travis-ci/setup_env.sh
+++ devtools/travis-ci/setup_env.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-# adapted from pytraj
-
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then
     wget http://repo.continuum.io/miniconda/Miniconda-3.7.0-MacOSX-x86_64.sh -O miniconda.sh;
 else
@@ -17,9 +15,9 @@ pip install conda
 conda install --yes conda-build jinja2 anaconda-client pip
 
 # create myenv
-conda create -y -n myenv python=$PYTHON_VERSION numpy notebook
+conda create -y -n myenv python=$PYTHON_VERSION jupyter notebook
 
 source activate myenv
-conda install --yes anaconda-client coverage pyflakes
-conda install mdtraj -c omnia --yes
-pip install mdanalysis
+# conda install mdtraj -c omnia --yes
+# conda install pytraj-dev -c ambermd --yes
+# pip install mdanalysis
