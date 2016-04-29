04250fb13b7cffe9b515708f3700aa6596429829
Hai Nguyen <hainm.comp@gmail.com>
2016-04-29 02:21:21 -0400

    fix

--- setup.py
+++ setup.py
@@ -40,10 +40,9 @@ if __name__ == '__main__':
             "nglview.html": ["static/*"]
         },
         packages=["nglview", "nglview.datafiles", "nglview.html"],
-        requires=["jupyter"],
+        install_requires=["jupyter", "traitlets>=4.2.1"],
         tests_require=["nose"],
         test_suite="nose.collector",
-        install_requires=["jupyter", "traitlets>=4.2.1"]
         extras_require={
             "simpletraj": ["simpletraj"],
             "mdtraj": ["mdtraj"],
