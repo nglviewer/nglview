d1f509e1fdc2b980102aadf8ee082bcb11751875
Hai Nguyen <hainm.comp@gmail.com>
2016-05-02 13:58:37 -0400

    add cython for mda

--- devtools/travis-ci/setup_env.sh
+++ devtools/travis-ci/setup_env.sh
@@ -15,7 +15,7 @@ pip install conda
 conda install --yes conda-build jinja2 anaconda-client pip
 
 # create myenv
-conda create -y -n myenv python=$PYTHON_VERSION jupyter notebook nose numpy mock coverage
+conda create -y -n myenv python=$PYTHON_VERSION jupyter notebook nose numpy mock coverage cython
 
 source activate myenv
 pip install pytraj
