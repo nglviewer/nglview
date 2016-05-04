38387b9860698b84a3612b56b2fa8da5a8f1207b
Hai Nguyen <hainm@users.noreply.github.com>
2016-05-04 15:30:12 -0400

    create tests for notebook (#106)
    * add clone_nbtest.sh
    
    * try nbtest
    
    * add test.js
    
    * turn off mda for now, too slow
    
    * fix typos
    
    * trigger rebuild
    
    * try write less
    
    * try
    
    * fix nb
    
    * more nb
    
    * turn on MDA now

---
+++ devtools/travis-ci/clone_nbtest.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+git clone https://github.com/hainm/nbtests
+cp -r nbtests/* .
---
+++ nglview/tests/js/test.js
@@ -0,0 +1,24 @@
+var testsuits = {};
+
+// TODO: auto-discovery
+// var nbfiles = ["repr_alias.ipynb", "init_representations.ipynb"]; 
+var nbfiles = ["caching.ipynb", "representations.ipynb", "closest_waters.ipynb"];
+
+for (var i = 0; i < nbfiles.length; i++) {
+    var fn = nbfiles[i];
+
+    testsuits[fn] = function (browser) {
+        browser.openNotebook(fn);
+        
+        browser.restartKernel(2000);
+        for (var i = 0; i < 23 ; i++) {
+           browser.executeCell(i)
+                  .pause(1000)
+                  .cellHasError(i);
+        }
+        
+        browser.end();
+    }
+}
+
+module.exports = testsuits;
