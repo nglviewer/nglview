2add28feeafb3c311533172debc72e4ee4f92f45
Hai Nguyen <hainm.comp@gmail.com>
2016-05-04 15:14:09 -0400

    more nb

--- nglview/tests/js/test.js
+++ nglview/tests/js/test.js
@@ -2,7 +2,7 @@ var testsuits = {};
 
 // TODO: auto-discovery
 // var nbfiles = ["repr_alias.ipynb", "init_representations.ipynb"]; 
-var nbfiles = ["caching.ipynb"];
+var nbfiles = ["caching.ipynb", "representations.ipynb", "closest_waters.ipynb"];
 
 for (var i = 0; i < nbfiles.length; i++) {
     var fn = nbfiles[i];
