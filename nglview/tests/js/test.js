031b762cb99a0760245cf4387ab88c5fd25d6f49
Hai Nguyen <hainm.comp@gmail.com>
2016-05-04 14:16:45 -0400

    add test.js

---
+++ nglview/tests/js/test.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "Test repr_alias": function (browser) {
+        browser.openNotebook("repr_alias.ipynb");
+        
+        browser.restartKernel(2000);
+        for ( var i = 0; i < 23 ; i++) {
+           browser.executeCell(i)
+                  .pause(1000)
+                  .cellHasError(i);
+        }
+        
+        browser.end();
+    }
+}
