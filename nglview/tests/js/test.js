e8e25227b6822230596ccfc6e56eee3418d38869
Hai Nguyen <hainm.comp@gmail.com>
2016-05-04 14:31:50 -0400

    trigger rebuild

--- nglview/tests/js/test.js
+++ nglview/tests/js/test.js
@@ -10,5 +10,18 @@ module.exports = {
         }
         
         browser.end();
+    },
+
+    "Test initialize representation": function (browser) {
+        browser.openNotebook("init_representations.ipynb");
+        
+        browser.restartKernel(2000);
+        for ( var i = 0; i < 23 ; i++) {
+           browser.executeCell(i)
+                  .pause(1000)
+                  .cellHasError(i);
+        }
+        
+        browser.end();
     }
 }
