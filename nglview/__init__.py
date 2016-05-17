2ccfeca9f768b7b46aa8aab535a52054f65cc447
Hai Nguyen <hainm.comp@gmail.com>
2016-05-17 17:07:32 -0400

    rename export_image to render_image

--- nglview/__init__.py
+++ nglview/__init__.py
@@ -838,7 +838,7 @@ class NGLWidget(widgets.DOMWidget):
 
     @observe('_image_data')
     def get_image(self, change=""):
-        '''get rendered image. Make sure to call `export_image` first
+        '''get rendered image. Make sure to call `render_image` first
 
         Notes
         -----
@@ -848,7 +848,7 @@ class NGLWidget(widgets.DOMWidget):
         image._b64value = self._image_data
         return image
 
-    def export_image(self, frame=None,
+    def render_image(self, frame=None,
                      factor=4,
                      antialias=True,
                      trim=False,
@@ -869,14 +869,14 @@ class NGLWidget(widgets.DOMWidget):
         Examples
         --------
             # tell NGL to render send image data to notebook.
-            view.export_image()
+            view.render_image()
             
             # make sure to call `get_image` method
             view.get_image()
 
         Notes
         -----
-        You need to call `export_image` and `get_image` in different notebook's Cells
+        You need to call `render_image` and `get_image` in different notebook's Cells
         """
         if frame is not None:
             self.frame = frame
