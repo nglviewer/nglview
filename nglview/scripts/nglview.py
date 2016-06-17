462587e3eeb184bfe555e9389ee92e0b73bdf19f
Hai Nguyen <hainm.comp@gmail.com>
2016-06-17 18:08:20 -0400

    Revert "add help for remote nglview (#284)"
    This reverts commit 497bc1449d0e5061e0445b98cb9bc5e42fd2dc98.

--- nglview/scripts/nglview.py
+++ nglview/scripts/nglview.py
@@ -1,43 +1,13 @@
 #!/usr/bin/env python
-import os, sys, argparse
+import os, sys, argparse, json
 
-remote_msg = """
-SSH port forwarding help
-
-    In your remote machine
-    ----------------------
-    
-        export port=8890 # if port=8890 is not available, pick another one
-        jupyter notebook --port=$port --no-browser
-
-    
-    In your local machine
-    ---------------------
-    
-        export port=8890 #  same as given port in your remote machine
-        ssh -N -f -L localhost:$port:localhost:$port {username}@{hostname}
-    
-    Then open your favorite web browser, paste
-    
-        localhost:8890
-    
-        # Note: change 8890 to the port number you specified
-
-
-Troubleshooting:
-
-    If you get "bind: Address already in use", please issue another port number
-
-"""
-
-notebook_content = r"""
-{
+notebook_dict = {
  "cells": [
   {
    "cell_type": "code",
-   "execution_count": null,
+   "execution_count": 'null',
    "metadata": {
-    "collapsed": true
+    "collapsed": True
    },
    "outputs": [],
    "source": [
@@ -65,23 +35,16 @@ notebook_content = r"""
    "mimetype": "text/x-python",
    "name": "python",
    "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
+   "pygments_lexer": "ipython",
    "version": "3.5.1"
   }
  },
  "nbformat": 4,
  "nbformat_minor": 0
 }
-""".strip()
 
-def help_remote(remote_msg=remote_msg):
-    import os, socket
-    username = os.getlogin()
-    hostname = socket.gethostname()
-
-    print(remote_msg.format(username=username, hostname=hostname))
-
-def main(notebook_content=notebook_content):
+def main(notebook_dict=notebook_dict):
+    PY3 = sys.version_info[0] == 3
     parser = argparse.ArgumentParser(description='NGLView')
     # parser.add_argument('-p', '--parm', help='Topology filename', required=True)
     parser.add_argument('parm', help='Topology filename (could be PDB, CIF, ... files)') 
@@ -96,23 +59,25 @@ def main(notebook_content=notebook_content):
     if crd is None:
         crd = parm
 
-    if parm.lower() == 'remote':
-        help_remote()
+    browser = '--browser ' + args.browser if args.browser else ''
+
+    if parm.endswith('.ipynb'):
+        notebook_name = parm
     else:
-        browser = '--browser ' + args.browser if args.browser else ''
-
         notebook_name = 'tmpnb_ngl.ipynb'
-        notebook_content = notebook_content.replace('test.nc', crd).replace('prmtop', parm)
-
+        nb_json = json.dumps(notebook_dict)
+        nb_json = nb_json.replace('"null"', 'null').replace('test.nc', crd).replace('prmtop', parm)
+        if not PY3:
+           nb_json.replace('python3', 'python')
         with open(notebook_name, 'w') as fh:
-            fh.write(notebook_content)
-        
-        
-        cm = '{jupyter} notebook {notebook_name} {browser}'.format(jupyter=args.jexe,
-                                                                   notebook_name=notebook_name,
-                                                                   browser=browser)
-        print(cm)
-        os.system(cm)
+            fh.write(nb_json)
+    
+    
+    cm = '{jupyter} notebook {notebook_name} {browser}'.format(jupyter=args.jexe,
+                                                               notebook_name=notebook_name,
+                                                               browser=browser)
+    print(cm)
+    os.system(cm)
 
 if __name__ == '__main__':
     main()
