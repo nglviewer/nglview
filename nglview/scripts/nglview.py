e32eda19928ddb71dea0fad5f7b391849a820e3a
Hai Nguyen <hainm@users.noreply.github.com>
2016-06-18 01:28:36 -0400

    able specify notebook name + choose correct kernel (#291)
    * option to specify notebook name
    
    * choose correct kernel too

--- nglview/scripts/nglview.py
+++ nglview/scripts/nglview.py
@@ -2,6 +2,8 @@
 import os, sys, argparse, json
 import subprocess
 
+bin_path = sys.prefix + '/bin/'
+
 remote_msg = """
 SSH port forwarding help
     In your remote machine
@@ -88,12 +90,17 @@ def install_nbextension(jupyter):
 
 def main(notebook_dict=notebook_dict):
     PY3 = sys.version_info[0] == 3
+    pyv_full_string = ','.join(str(i) for i in sys.version_info)
+    pyv_short_string = str(sys.version_info[0])
+    default_jexe = bin_path + 'jupyter'
+
     parser = argparse.ArgumentParser(description='NGLView')
     # parser.add_argument('-p', '--parm', help='Topology filename', required=True)
     parser.add_argument('parm', help='Topology filename (could be PDB, CIF, ... files)') 
     parser.add_argument('-c', '--crd', help='Coordinate filename')
     parser.add_argument('--browser', help='web browser, optional')
-    parser.add_argument('-j', '--jexe', default='jupyter', help='jupyter command, optional')
+    parser.add_argument('-j', '--jexe', default=default_jexe, help='jupyter command, optional')
+    parser.add_argument('--notebook-name', default='tmpnb_ngl.ipynb', help='notebook name, optional')
     args = parser.parse_args()
 
     parm = args.parm
@@ -110,7 +117,15 @@ def main(notebook_dict=notebook_dict):
         if parm.endswith('.ipynb'):
             notebook_name = parm
         else:
-            notebook_name = 'tmpnb_ngl.ipynb'
+            notebook_name = args.notebook_name
+            if not PY3:
+                kernelspec = notebook_dict['metadata']['kernelspec']
+                kernelspec['display_name'] = 'Python 2'
+                kernelspec['name'] = 'python2'
+                notebook_dict['metadata']['kernelspec'] = kernelspec
+
+                codemirror_mode = notebook_dict['metadata']['language_info']['codemirror_mode']['version'] = pyv_short_string
+                notebook_dict['metadata']['version'] = pyv_full_string
             if parm.endswith('.py'):
                 pycontent = open(parm).read().strip()
                 notebook_dict['cells'][0]['source'] = pycontent
@@ -118,9 +133,6 @@ def main(notebook_dict=notebook_dict):
             else:
                 nb_json = json.dumps(notebook_dict)
                 nb_json = nb_json.replace('"null"', 'null').replace('test.nc', crd).replace('prmtop', parm)
-            if not PY3:
-               nb_json.replace('python3', 'python')
-
             nb_json = nb_json.replace('"null"', 'null')
 
             with open(notebook_name, 'w') as fh:
