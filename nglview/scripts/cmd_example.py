e39d87634965b6f6822f42003f9f77f2801d54dd
Hai Nguyen <hainm.comp@gmail.com>
2016-06-18 21:56:31 -0400

    more doc

---
+++ nglview/scripts/cmd_example.py
@@ -0,0 +1,20 @@
+CMD_EXAMPLE = """Example
+
+    # open notebook and display pdb file
+    nglview 1tsu.pdb
+    
+    # open notebook and display trajectory
+    nglview 1tsu.parm7 -c traj.nc
+
+    # open notebook and display trajectory by reading all files ending with .nc
+    nglview 1tsu.parm7 -c *.nc
+    
+    # open notebook and copy myscript.py content to 1st cell
+    nglview myscript.py
+    
+    # open my_notebook.ipynb notebook and run 1st cell
+    nglview my_notebook.ipynb
+    
+    # display instruction about using remote notebook
+    nglview remote
+"""
