8cab3404c627723698abf837fe30be71d7730039
Hai Nguyen <hainm.comp@gmail.com>
2016-07-03 21:38:18 -0400

    update

--- nglview/theme/jsutils.py
+++ nglview/theme/jsutils.py
@@ -9,9 +9,22 @@ for (var i=0; i < output_area.length; i++){
 """
 
 js_launch_qtconsole = """
-IPython.notebook.kernel.execute('%qtconsole')
+Jupyter.notebook.kernel.execute('%qtconsole')
 """
 
 js_open_url_template = """
 window.open({url});
+"""
+
+js_clean_error_output = """
+var cells = Jupyter.notebook.get_cells();
+
+for (var i =0; i < cells.length; i++){
+    var cell = cells[i];
+    if (cell.output_area.outputs.length > 0) {
+        var out = cell.output_area.outputs[0];
+        if (out.output_type == 'error') {
+            cell.clear_output()
+        }
+}
 """
