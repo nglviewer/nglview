2f1e649e3c12e4241e562a30285968da5586abb7
Hai Nguyen <hainm.comp@gmail.com>
2016-07-05 18:06:50 -0400

    add --browser=google-chrome

--- devtools/travis-ci/open_notebook.sh
+++ devtools/travis-ci/open_notebook.sh
@@ -2,7 +2,7 @@
 
 if [[ "$PYTHON_VERSION" == "3.5" && "$TRAVIS_OS_NAME" == "osx" ]]; then
     sleep 3
-    jupyter notebook --port=8889 &
+    jupyter notebook --port=8889 --browser=google-chrome &
     sleep 3
     nightwatch
 fi
