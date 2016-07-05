a322f3068308d7e10d57dc017998eebb3c1e3284
Hai Nguyen <hainm.comp@gmail.com>
2016-07-05 18:27:17 -0400

    try more

--- devtools/travis-ci/open_notebook.sh
+++ devtools/travis-ci/open_notebook.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [[ "$PYTHON_VERSION" == "3.5" && "$TRAVIS_OS_NAME" == "osx" ]]; then
+if [[ "$PYTHON_VERSION" == "3.5" ]]; then
     sleep 3
     jupyter notebook --port=8889 --browser=google-chrome &
     sleep 3
