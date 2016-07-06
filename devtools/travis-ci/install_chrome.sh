1e5101074b54f247f0cfb0f24ab2b8daf231d2ce
Hai Nguyen <hainm.comp@gmail.com>
2016-07-06 14:20:18 -0400

    add chromedriver

--- devtools/travis-ci/install_chrome.sh
+++ devtools/travis-ci/install_chrome.sh
@@ -5,3 +5,9 @@ sudo apt-get update
 sudo apt-get install -y libappindicator1 fonts-liberation
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
+
+# chromedriver
+wget http://chromedriver.storage.googleapis.com/2.21/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+sudo chmod u+x chromedriver
+sudo mv chromedriver /usr/bin/
