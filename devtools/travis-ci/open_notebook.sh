08e352d410be35ec00522047091851a1229bdc83
Hai Nguyen <hainm.comp@gmail.com>
2016-07-06 15:43:57 -0400

    remove sudo ... jupyter

--- devtools/travis-ci/open_notebook.sh
+++ devtools/travis-ci/open_notebook.sh
@@ -2,7 +2,8 @@
 
 if [[ "$PYTHON_VERSION" == "3.5" ]]; then
     sleep 3
-    sudo /home/travis/miniconda/envs/myenv/bin/jupyter notebook --port=8889 --browser=google-chrome --ip=127.0.0.1 &
+    # sudo /home/travis/miniconda/envs/myenv/bin/jupyter notebook --port=8889 --browser=google-chrome --ip=127.0.0.1 &
+    jupyter notebook --port=8889 --browser=google-chrome --ip=127.0.0.1 &
     sleep 3
     nightwatch
 fi
