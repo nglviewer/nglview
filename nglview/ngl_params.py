93256c80829a5bfa202385527a83072edabecd66
Hai Nguyen <hainm@users.noreply.github.com>
2016-07-10 23:35:05 -0400

    real time update repr and selection (#366)
    * real time update repr and selection
    
    * Realtime (#17)
    
    * add ngl_params
    
    * more

---
+++ nglview/ngl_params.py
@@ -0,0 +1,22 @@
+REPR_NAME_PAIRS = [
+        ('point', 'point'),
+        ('line', 'line'),
+        ('rope', 'rope'),
+        ('tube', 'tube'),
+        ('trace', 'trace'),
+        ('label', 'label'),
+        ('unitcell', 'unitcell'),
+        ('cartoon', 'cartoon'),
+        ('licorice', 'licorice'),
+        ('ribbon', 'ribbon'),
+        ('surface', 'surface'),
+        ('backbone', 'backbone'),
+        ('contact', 'contact'),
+        ('hyperball', 'hyperball'),
+        ('rocket', 'rocket'),
+        ('helixorient', 'helixorient'),
+        ('simplified_base', 'base'),
+        ('ball_and_stick', 'ball+stick'),
+        ]
+
+REPR_NAMES = set([name for pairs in REPR_NAME_PAIRS for name in pairs])
