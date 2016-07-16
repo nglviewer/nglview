5bb32de1f5b15ab3cc41010306ae70158134ff88
Hai Nguyen <hainm.comp@gmail.com>
2016-07-16 14:11:23 -0400

    try

--- devtools/make_test_js.py
+++ devtools/make_test_js.py
@@ -17,11 +17,11 @@ api_root_dir = 'nglview/tests/notebooks/api/'
 
 if args.basic:
     notebook_names  = [
+                 'test_no_gui_demo.ipynb',
                  'test_component_names.ipynb',
                  'test_component_dropdown_options.ipynb',
                  'test_add_structure_then_trajectory.ipynb',
                  'test_sync_n_components.ipynb',
-                 'test_no_gui_demo.ipynb',
                  'test_add_structure_then_trajectory.ipynb',
                  'test_automatically_added_attributes_0.ipynb',
                  # 'binary_vs_base64.ipynb',
