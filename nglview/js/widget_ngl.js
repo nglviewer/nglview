c39131194cd62205a322530de7e7f3e1ff23e844
Hai Nguyen <hainm.comp@gmail.com>
2016-07-17 13:05:24 -0400

    update ngl dev

--- nglview/js/widget_ngl.js
+++ nglview/js/widget_ngl.js
@@ -1,49 +1,11 @@
-
-require.config( {
-    paths: {
-        "THREE": "../nbextensions/nglview/three.custom.min",
-        "Promise": "../nbextensions/nglview/promise.min",
-        "sprintf": "../nbextensions/nglview/sprintf.min",
-        "pako": "../nbextensions/nglview/pako_inflate.min",
-        "chroma": "../nbextensions/nglview/chroma.min",
-        "jsfeat": "../nbextensions/nglview/svd.min",
-        "signals": "../nbextensions/nglview/signals.min",
-        "msgpackDecode": "../nbextensions/nglview/msgpack-decode",
-        "mmtfDecode": "../nbextensions/nglview/mmtf-decode",
-        "TypedFastBitSet": "../nbextensions/nglview/TypedFastBitSet",
-        "NGL": "../nbextensions/nglview/ngl"
-    },
-    shim: {
-        THREE: { exports: "THREE" },
-        Promise: { exports: "Promise" },
-        sprintf: { exports: "sprintf" },
-        jsfeat: { exports: "jsfeat" },
-        msgpackDecode: { exports: "msgpackDecode" },
-        mmtfDecode: { exports: "mmtfDecode" },
-        NGL: {
-            deps: [
-                "THREE", "Promise", "sprintf", "pako", "chroma", "jsfeat",
-                "signals", "msgpackDecode", "mmtfDecode", "TypedFastBitSet"
-            ],
-            exports: "NGL"
-        }
-    },
-} );
-
-define( [
+define([
     "nbextensions/widgets/widgets/js/widget",
     "nbextensions/widgets/widgets/js/manager",
     "jqueryui",
-    "THREE", "Promise", "sprintf", "pako", "chroma", "jsfeat", "signals",
-    "msgpackDecode", "mmtfDecode", "TypedFastBitSet", "NGL"
+    "nbextensions/nglview/ngl"
 ], function(
-    widget, manager, $, _THREE, _Promise, _sprintf, pako, chroma, _jsfeat,
-    signals, _msgpackDecode, _mmtfDecode, _TypedFastBitSet, _NGL
+    widget, manager, $, NGL
 ){
-
-    window.pako = pako;
-    window.signals = signals;
-    window.chroma = chroma;
 
     var NGLView = widget.DOMWidgetView.extend( {
 
