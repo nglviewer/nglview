4c9d754ee99411db176ca90d4e4b6592b2b99f48
Hai Nguyen <hainm.comp@gmail.com>
2016-07-30 03:56:23 -0400

    notebook

--- nglview/__init__.py
+++ nglview/__init__.py
@@ -1677,11 +1677,15 @@ class Shape(object):
 
     Examples
     --------
-    >>> import nglview sa nv
+    >>> import nglview as nv
     >>> view = nv.NGLWidget()
     >>> view
     >>> shape = nv.Shape(view=view)
     >>> shape.add_sphere(from=[0, 0, 0], to=[10, 10, 10], color=[1, 0, 0], radius=1.5])
+
+    See also
+    --------
+    http://arose.github.io/ngl/api/dev/Shape.html
     """
 
     def __init__(self, view):
@@ -1691,15 +1695,17 @@ class Shape(object):
 
     def _make_func(self, names):
         from types import MethodType
+
+        def make_func(name):
+            def func(this, *args):
+                args_with_name = [name, ] + list(args)
+                self.add(*args_with_name)
+            func.__doc__ = 'check `add` method'
+            return func
+
         for name in names:
-            def make_func():
-                def func(this, *args):
-                    args_with_name = [name, ] + list(args)
-                    self.add(*args_with_name)
-                func.__doc__ = 'check `add` method'
-                return func
             func_name = 'add_' + name
-            func = make_func()
+            func = make_func(name)
             setattr(self, func_name, MethodType(func, self))
 
     def add(self, *args):
