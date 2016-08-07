85bbfa1d320cb5377e6de5e142230744b8e69d00
Hai Nguyen <hainm.comp@gmail.com>
2016-08-06 20:29:41 -0400

    add layout

--- nglview/player.py
+++ nglview/player.py
@@ -3,7 +3,6 @@
 from __future__ import absolute_import
 import time
 import json
-import numpy as np
 from IPython.display import display, Javascript
 from ipywidgets import (DOMWidget,
                         Box, HBox, VBox, Checkbox,
@@ -13,109 +12,17 @@ from ipywidgets import (DOMWidget,
                         Text, Textarea, IntText, FloatText,
                         Label,
                         interactive,
-                        Tab, Layout, Accordion)
+                        Layout, Accordion)
 
-from traitlets import Int, Bool, Dict, Float, CaselessStrEnum, TraitError
+from traitlets import Int, Bool, Dict, Float, CaselessStrEnum
 from traitlets import observe, link
 
 from .ngl_params import REPR_NAMES
-from .widget_utils import get_widget_by_name
 from . import default
 from . import jsutils
+from .layout import (form_item_layout, _relayout, _make_autofit, _relayout_master, _make_delay_tab,
+        _make_box_layout)
 
-try:
-    form_item_layout = Layout(
-        display='flex',
-        flex_flow='row',
-        justify_content='space-between'
-    )
-except TraitError:
-    # for testing
-    form_item_layout = None
-
-def _make_box_layout(width='100%'): 
-    return Layout(
-        display='flex',
-        flex_flow='column',
-        align_items='stretch',
-        width=width)
-
-def _relayout(box, form_item_layout):
-    form_items = []
-    for kid in box.children:
-        if hasattr(kid, 'description') and not isinstance(kid, (Button, ToggleButton)):
-            label_value = kid.description
-            kid.description = ''
-        else:
-            label_value = ''
-        if isinstance(kid, Button):
-            box2 = Box([kid,], layout=form_item_layout)
-        else:
-            box2 = Box([Label(value=label_value), kid], layout=form_item_layout)
-        form_items.append(box2)
-
-    return form_items
-
-def _relayout_master(box, width='20%'):
-    """make nicer layout for box.
-
-    This method will take the `description` of each child to make corresponding Label
-    The `description` will be cleared.
-    """
-    old_children = box.children[:]
-    form_items = _relayout(box, form_item_layout)
-    form = Box(form_items, layout=_make_box_layout(width=width))
-    form._ngl_children = old_children
-    return form
-
-def _make_autofit(box):
-    '''
-
-    Parameters
-    ----------
-    box : ipywidgets.Box
-        children is a list of buttons
-
-    Returns
-    -------
-    relayouted box
-    '''
-    items_layout = Layout(flex='1 1 auto',
-                          width='auto')  
-
-    box.layout = items_layout
-    return box
-
-def _make_delay_tab(box_factory, selected_index=0):
-    """
-
-    Parameters
-    ----------
-    box_factory : list of (func, tab_name)
-
-    Example of box_factory: [(_make_gen_box, 'General'),
-                             (_make_repr_box, 'Representation')]
-    """
-
-    tab = Tab([Box() for box, _ in box_factory])
-    [tab.set_title(i, title) for i, (_, title) in enumerate(box_factory)]
-
-    # trick
-    if not tab.children[selected_index].children:
-        tab.selected_index = -1
-
-    def on_update_selected_index(change):
-        index = change['new']
-        if not tab.children[index].children:
-            # make widget on demand
-            tab.children[index].children = [box_factory[index][0](),]
-
-    tab.observe(on_update_selected_index, names='selected_index')
-
-    # trigger
-    tab.selected_index = selected_index
-
-    return tab
 
 class TrajectoryPlayer(DOMWidget):
     # should set default values here different from desired defaults
@@ -417,14 +324,6 @@ class TrajectoryPlayer(DOMWidget):
         def on_click_refresh(button):
             self._refresh(component_slider, repr_slider)
 
-        def on_click_update(button):
-            parameters = json.loads(repr_text_info.value.replace("False", "false").replace("True", "true"))
-            self._view.update_representation(component=component_slider.value,
-                                             repr_index=repr_slider.value,
-                                             **parameters)
-            self._view._set_selection(repr_selection.value,
-                                      component=component_slider.value,
-                                      repr_index=repr_slider.value)
         def on_click_remove(button_remove):
             self._view._remove_representation(component=component_slider.value,
                                               repr_index=repr_slider.value)
