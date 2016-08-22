3ff2007291a37a68876338a8eb4cd3c47b550b31
Hai Nguyen <hainm.comp@gmail.com>
2016-08-22 15:07:39 -0400

    doc

--- nglview/adaptor.py
+++ nglview/adaptor.py
@@ -95,7 +95,8 @@ class PdbIdStructure(Structure):
 
 
 class Trajectory(object):
-
+    """abstract base class
+    """
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.shown = True
