6b95cfcebe6a384f8d80d3b3dfd555906c2a1776
Hai Nguyen <hainm.comp@gmail.com>
2016-08-25 19:20:52 -0400

    fix test

--- nglview/tests/test_widget.py
+++ nglview/tests/test_widget.py
@@ -542,13 +542,13 @@ def test_player_simple():
     player._update_padding()
     player.on_spin_changed(change=dict(new=True))
     player.on_spin_x_changed(change=dict(new=1))
-    player.on_spin_y_changed(change=dict(new=1)
+    player.on_spin_y_changed(change=dict(new=1))
     player.on_spin_z_changed(change=dict(new=1))
     player.on_spin_speed_changed(change=dict(new=0.5))
     player._real_time_update = True
     player._make_widget_repr()
-    player.component_slider
-    player.repr_slider
+    player.widget_component_slider
+    player.widget_repr_slider
 
 def test_player_link_to_ipywidgets():
     traj = pt.datafiles.load_tz2()
