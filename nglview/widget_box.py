5abbb7934a423d3119f7e0b483f86fea0f7b3543
Hai Nguyen <hainm.comp@gmail.com>
2016-08-27 00:42:33 -0400

    update lyaout

--- nglview/widget_box.py
+++ nglview/widget_box.py
@@ -4,19 +4,28 @@ from .widget import NGLWidget
 from .layout import form_item_layout
 from . import default
 from .utils import js_utils
+from traitlets import CaselessStrEnum, observe
 
-class HBoxNGL(Box):
+class BoxNGL(Box):
+    _gui_style = CaselessStrEnum(['row', 'column'], default_value='row').tag(sync=True)
+
     def __init__(self, *args, **kwargs):
-        super(HBoxNGL, self).__init__(*args, **kwargs)
+        super(BoxNGL, self).__init__(*args, **kwargs)
 
         self.layout = form_item_layout
+
+    @observe('_gui_style')
+    def _update_gui_style(self, change):
+        """row or column style
+        """
+        what = change['new']
+        self.layout.flex_flow = what.lower()
 
     def _ipython_display_(self, *args, **kwargs):
         for widget in self.children:
             if isinstance(widget, NGLWidget):
                 widget.displayed = True
-        js_utils._set_notebook_width('60%', left_padding=None)
-        super(HBoxNGL, self)._ipython_display_(*args, **kwargs)
+        super(BoxNGL, self)._ipython_display_(*args, **kwargs)
 
     def _update_padding(self, padding=default.DEFAULT_PADDING):
         for widget in self.children:
@@ -30,5 +39,6 @@ class HBoxNGL(Box):
                   widget._remote_call('setSize', target='Widget', args=['700px', '500px'])
 
     def _beautify(self):
+        js_utils._set_notebook_width('60%', left_padding=None)
         self._update_padding()
         self._update_size()
