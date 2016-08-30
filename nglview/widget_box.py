102f622600ed52a152b82e9f499a77c59e4325d7
Hai Nguyen <hainm.comp@gmail.com>
2016-08-29 21:33:33 -0400

    change name

--- nglview/widget_box.py
+++ nglview/widget_box.py
@@ -36,7 +36,7 @@ class BoxNGL(Box):
         js_utils._set_notebook_width('60%', left_padding=None)
         self._update_size()
 
-class Box2(Box):
+class DraggableBox(Box):
     _view_name = Unicode("NGLBox").tag(sync=True)
     _view_module = Unicode("nglview-js").tag(sync=True)
     _dialog = Unicode('off').tag(sync=True)
