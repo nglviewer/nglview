44950e425c4a65c8af211dde2e8845e6b4ca10eb
Hai Nguyen <hainm.comp@gmail.com>
2016-09-01 19:38:54 -0400

    fix

--- nglview/player.py
+++ nglview/player.py
@@ -593,7 +593,20 @@ class TrajectoryPlayer(DOMWidget):
                 = filename_text.layout.max_width = delay_text.layout.max_width = default.DEFAULT_TEXT_WIDTH
 
         button_movie_images = Button(description='Export Images')
-        button_movie_images.on_click(self.on_click_images)
+        def download_image(filename):
+            self._view.download_image(factor=slider_factor.value,
+                    antialias=checkbox_antialias.value,
+                    trim=checkbox_trim.value,
+                    transparent=checkbox_transparent.value,
+                    filename=filename)
+
+        @button_movie_images.on_click
+        def on_click_images(button_movie_images):
+            for i in range(start_text.value, stop_text.value, step_text.value):
+                self._view.frame = i
+                time.sleep(delay_text.value)
+                download_image(filename=filename_text.value + str(i))
+                time.sleep(delay_text.value)
 
         vbox = VBox([
             button_movie_images,
@@ -874,17 +887,3 @@ class TrajectoryPlayer(DOMWidget):
                 widget.layout.display = 'none'
         self.widget_repr_choices.layout.display = 'flex'
         self.widget_accordion_repr_parameters.selected_index = 0
-
-    def download_image(self, filename):
-        self._view.download_image(factor=slider_factor.value,
-                antialias=checkbox_antialias.value,
-                trim=checkbox_trim.value,
-                transparent=checkbox_transparent.value,
-                filename=filename)
-
-    def on_click_images(self, button_movie_images):
-        for i in range(start_text.value, stop_text.value, step_text.value):
-            self._view.frame = i
-            time.sleep(delay_text.value)
-            self.download_image(filename=filename_text.value + str(i))
-            time.sleep(delay_text.value)
