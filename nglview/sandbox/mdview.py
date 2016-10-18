eadfe80305fe5987fee98e808165cd2449f6e159
Hai Nguyen <hainm.comp@gmail.com>
2016-10-18 17:04:49 -0400

    update

--- nglview/sandbox/mdview.py
+++ nglview/sandbox/mdview.py
@@ -2,8 +2,19 @@ import os
 import time
 import pytraj as pt
 from threading import Thread
+import abc, six
 
-class AmberMD(object):
+@six.add_metaclass(abc.ABCMeta)
+class BaseMD(object):
+    @abc.abstractmethod
+    def initialize(self):
+        pass
+
+    @abc.abstractmethod
+    def update(self):
+        pass
+
+class AmberMD(BaseMD):
     # TODO: doc
     """
 
@@ -23,25 +34,39 @@ class AmberMD(object):
         assert os.path.exists(reference), '{} must exists'.format(reference)
         self.restart = restart
         self.reference_traj = pt.load(reference, top=self.top)
-        self.thread = None
 
     def initialize(self, gui=False):
         self.view = self.reference_traj.visualize(gui=gui)
         return self.view
 
-    def update(self, every=1, autoimage=False, timeout=3000):
+    def update(self, every=1, timeout=3000, callback=None):
+        """
 
+        Parameters
+        ----------
+        every : int, default 1 (s)
+            update coordinates "every" second
+        timeout : int, default 3000 (s)
+            stop updating coordinate after "timeout"
+        callback : func, optional
+            If given, trajectory will be processed (autoimage, ...)
+            Must follow func(traj)
+        """
         def _update():
             start = time.time()
             while time.time() - start <= timeout:
                 time.sleep(every)
-                if autoimage:
-                    traj.autoimage()
                 traj = pt.load(self.restart, top=self.top)
-                mask = '@C,N,O,CA,P'
-                pt.superpose(traj, mask=mask, ref=self.reference_traj)
-                self.view.coordinates_dict = {0: traj[0].xyz}
+                if callback is not None:
+                    callback(traj)
+                else:
+                    mask = '@C,N,O,CA,P'
+                    pt.superpose(traj, mask=mask, ref=self.reference_traj)
+                self._update_coordinates(traj[0].xyz)
         # non-blocking so we can use other Jupyter's cells
         self.thread = Thread(target=_update)
         self.thread.daemon = True
         self.thread.start()
+
+    def _update_coordinates(self, xyz):
+        self.view.coordinates_dict = {0: xyz}
