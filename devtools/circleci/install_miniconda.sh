abe40b2701c4e529ac3fdb0eda3b977de7ad4f65
Hai Nguyen <hainm.comp@gmail.com>
2016-11-21 17:34:31 -0500

    use conda from conda-forge

--- devtools/circleci/install_miniconda.sh
+++ devtools/circleci/install_miniconda.sh
@@ -4,10 +4,11 @@ yum install -y \
     wget \
     unzip
 
-miniconda=https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-wget $miniconda -O miniconda.sh
-bash miniconda.sh -b
-export PATH=/root/miniconda3/bin:$PATH
+# miniconda=https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+# wget $miniconda -O miniconda.sh
+# bash miniconda.sh -b
+# export PATH=/root/miniconda3/bin:$PATH
+export PATH=/opt/conda/bin/:$PATH
 conda update --yes --all
 conda install --yes conda-build anaconda-client numpy matplotlib
 conda info
