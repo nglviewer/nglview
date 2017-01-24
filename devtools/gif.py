8c00858926d3243060e5b8968dd4798f6b907b80
Hai Nguyen <hainm@users.noreply.github.com>
2017-01-24 16:56:41 -0500

    update gifipy: should use ffmpeg directly. (#569)
    * not use moviepy; use ffmpeg directly
    
    * rename

--- devtools/gif.py
+++
@@ -1,4 +0,0 @@
-from moviepy import editor
-
-x = editor.VideoFileClip('nglview.mov')
-x.write_gif("nglview.gif", fps=8, opt='nq')
---
+++ devtools/gifipy.py
@@ -0,0 +1,11 @@
+# require: ffmpeg
+# e.g: conda install ffmpeg -c menpo
+# 
+import sys
+import subprocess
+
+mov_in = sys.argv[1]
+mov_out = sys.argv[2]
+
+command = 'ffmpeg -i {} -pix_fmt rgb24 -r 10 -f gif {}'.format(mov_in, mov_out)
+subprocess.call(command, shell=True)
