c536489ade8ee5faed0232336bdfda072cef04e6
Hai Nguyen <hainm.comp@gmail.com>
2017-02-02 12:35:02 -0500

    run linter

--- devtools/travis-ci/run_tests.sh
+++ devtools/travis-ci/run_tests.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-if [[ "$TEST_NOTEBOOK" == "yes" ]]; then
+if [ "$TEST_NOTEBOOK" == "yes" ]; then
     sleep 3
     # sudo /home/travis/miniconda/envs/myenv/bin/jupyter notebook --port=8889 --browser=google-chrome --ip=127.0.0.1 &
     jupyter notebook --port=8889 --browser=google-chrome --ip=127.0.0.1 &
     sleep 3
     nightwatch
 else
-  (cd nglview/tests && py.test --cov=nglview -vs .) 
+  if [ "$TESTJS" == "True" ]; then
+      conda install nodejs -c conda-forge -y
+      (cd js/ && npm run lint); 
+  else
+      (cd nglview/tests && py.test --cov=nglview -vs .);
+  fi
 fi
