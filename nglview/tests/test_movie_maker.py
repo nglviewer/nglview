1c8261fefde7f93eb77081bee705cfcfc6ee0c22
HN <nhai.qn@gmail.com>
2017-03-26 01:47:10 -0400

    more test movie

--- nglview/tests/test_movie_maker.py
+++ nglview/tests/test_movie_maker.py
@@ -9,5 +9,19 @@ def test_movie_maker():
     traj = pytraj.datafiles.load_tz2()
     view = nglview.show_pytraj(traj)
 
-    movie = MovieMaker(view)
+    movie = MovieMaker(view, in_memory=False)
     movie.make()
+
+    movie = MovieMaker(view, in_memory=True)
+    movie.make()
+
+    movie.interupt()
+
+    movie = MovieMaker(view, in_memory=True, skip_render=True)
+    movie.make()
+
+    movie = MovieMaker(view, in_memory=True, skip_render=False,
+            start=0, stop=1, step=1)
+    movie.make()
+
+    print(movie.thread)
