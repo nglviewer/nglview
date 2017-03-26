d6bde20fc654881ae773481c84259f146f987c57
HN <nhai.qn@gmail.com>
2017-03-25 23:29:30 -0400

    mock more

--- nglview/tests/test_show.py
+++ nglview/tests/test_show.py
@@ -1,10 +1,12 @@
 import subprocess
 from mock import MagicMock, patch
 import nglview
+import numpy as np
 
 # local
 from make_dummy_comm import *
 # TODO : add more show_xxx
+# (check test_widgets.py)
 
 def _write(*args, **kargs):
     # fake write method
@@ -17,3 +19,13 @@ def test_show_schrodinger_structure():
     s = MagicMock()
     s.write = _write
     nglview.show_schrodinger_structure(s)
+
+
+def test_show_htmd():
+    mol = MagicMock()
+    mol.write = _write
+    n_frames = 10
+    mol.coordinates = np.zeros((n_frames, 1000, 3))
+    mol.numFrames = n_frames
+    view = nglview.show_htmd(mol)
+    view
