70f431fa398ccd762069ed600c7e0252439023d9
Hai Nguyen <hainm@users.noreply.github.com>
2017-06-27 22:59:36 -0400

    add stage.py (#628)

---
+++ nglview/stage.py
@@ -0,0 +1,13 @@
+from .utils.py_utils import _camelize_dict
+
+class Stage(object):
+    """
+    Try to mimic NGL.Stage
+    """
+
+    def __init__(self, view):
+        self._view = view
+
+    def set_parameters(self, **kwargs):
+        self._view._remote_call('setParameters', target='Stage',
+                kwargs=_camelize_dict(kwargs))
