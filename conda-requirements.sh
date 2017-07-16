ca5f0885b189e7d9e65068273b8272448cc0d62e
HN <nhai.qn@gmail.com>
2017-07-16 17:17:37 -0400

    requirement

--- conda-requirements.sh
+++ conda-requirements.sh
@@ -2,7 +2,7 @@
 
 # FIXME: Update to requirements.txt once those packages have binary
 # distribution on pypi.
-conda install mdanalysis -c conda-forge --yes
+conda install mdanalysis=0.16.2 -c conda-forge --yes
 conda install mdtraj=1.8.0 -c conda-forge --yes
 conda install rdkit -c rdkit --yes
 conda install simpletraj -c omnia --yes
