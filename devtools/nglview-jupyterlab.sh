4787c38a916ca432640fea8f6e53b2142b24d6dd
Hai Nguyen <hainm@users.noreply.github.com>
2017-07-21 00:13:37 -0400

    Instructions and commands to install nglview-jupyterlab (#649)
    * add devtools/nglview-jupyterlab.sh
    
    * more
    
    * docs for install jupyterlab
    
    * clean
    
    * update
    
    * add version

---
+++ devtools/nglview-jupyterlab.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+nglview_src=.
+env=jupyterlab_0252
+
+conda create -n $env python=3.6 numpy -y
+source activate $env
+
+pip install ipywidgets==7.0.0b0
+jupyter nbextension enable --py --sys-prefix widgetsnbextension
+
+conda install jupyterlab==0.25.2 -c conda-forge -y
+jupyter labextension install @jupyter-widgets/jupyterlab-manager # 0.24.7
+
+cd $nglview_src
+
+# nglview-js-widgets
+cd js
+npm install --save
+cd ../
+
+# nglview
+python setup.py install
+
+# nglview-jupyterlab
+jupyter-labextension install jslab
