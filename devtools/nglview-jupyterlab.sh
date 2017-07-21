bea435c5e71e1a745123618e772c56476e4efd20
HN <nhai.qn@gmail.com>
2017-07-20 23:57:54 -0400

    add devtools/nglview-jupyterlab.sh

---
+++ devtools/nglview-jupyterlab.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+nglview_src=.
+env=ipywidgets_test
+
+conda create -n $env python=3.6 numpy -y
+source activate $env
+
+pip install ipywidgets==7.0.0b0
+jupyter nbextension enable --py --sys-prefix widgetsnbextension
+
+conda install jupyterlab==0.25.2 -c conda-forge -y
+jupyter labextension install @jupyter-widgets/jupyterlab-manager
+
+cd $nglview_src
+git pull
+
+# nglview-js-widgets
+cd js
+npm install --save
+cd ../
+
+# nglview
+python setup.py install
+
+# nglview-jupyterlab
+jupyter-labextension install jslab
