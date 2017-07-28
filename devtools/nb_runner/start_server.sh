7a12680c9fa169dbd68fa28e239bcf9ccf06ee6f
HN <nhai.qn@gmail.com>
2017-07-27 23:21:10 -0400

    io

--- devtools/nb_runner/start_server.sh
+++ devtools/nb_runner/start_server.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo "Running notebook in background"
-jupyter notebook --port=8889 > /dev/null &
+jupyter notebook --port=8889 --NotebookApp.iopub_data_rate_limit 1000000000 > /dev/null &
 sleep 2
