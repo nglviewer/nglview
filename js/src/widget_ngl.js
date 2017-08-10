cb7c80f11942ab7d1a5c6cf68d72df266d947772
HN <nhai.qn@gmail.com>
2017-08-10 00:24:43 -0400

    Take version from package.json

--- js/src/widget_ngl.js
+++ js/src/widget_ngl.js
@@ -21,7 +21,7 @@ var NGLView = widgets.DOMWidgetView.extend({
         return _.extend(widgets.DOMWidgetModel.prototype.defaults(), {
             _view_name: "NGLView",
             _view_module: "nglview-js-widgets",
-            _view_module_version: "0.5.4-dev.8",
+            _view_module_version: require("../package.json").version,
         });
     },
 
