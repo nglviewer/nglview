ece580361cdcb821c6a5910c869015c6e26480d3
HN <nhai.qn@gmail.com>
2017-08-15 01:25:38 -0400

    add widgetsnbextension_version=3.0.0b12

--- devtools/nglview-jupyterlab.sh
+++ devtools/nglview-jupyterlab.sh
@@ -5,9 +5,9 @@ env=jupyterlab_0265
 jupyterlab_version=0.26.5
 python_version=3.6
 notebook_version=5.0.0
-ipywidgets_version=7.0.0b12
 lab_manager_version=0.25.11 # need to match to ./jslab/package.json
-# widgetsnbextension_version=3.0.0.b6
+ipywidgets_version=7.0.0b12
+widgetsnbextension_version=3.0.0b12
 
 echo "nglview root folder: $nglview_src"
 echo "Creating env $env"
@@ -17,7 +17,7 @@ source activate $env
 conda install setuptools -c conda-forge --force -y
 conda install notebook==${notebook_version} -c conda-forge -y
 pip install ipywidgets==${ipywidgets_version}
-# pip install widgetsnbextension==${widgetsnbextension} --upgrade
+pip install widgetsnbextension==${widgetsnbextension} --upgrade
 jupyter nbextension install --py --sys-prefix widgetsnbextension
 jupyter nbextension enable --py --sys-prefix widgetsnbextension
 
