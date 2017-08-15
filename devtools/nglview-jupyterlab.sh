f9a0d398e06790b03a7722806754e5372db121aa
HN <nhai.qn@gmail.com>
2017-08-14 22:21:13 -0400

    HIDE jupyterlab note

--- devtools/nglview-jupyterlab.sh
+++ devtools/nglview-jupyterlab.sh
@@ -2,16 +2,23 @@
 
 nglview_src=.
 env=jupyterlab_0252
+jupyterlab_version=0.25.2
+python_version=3.6
+notebook_version=5.0.0
+ipywidgets_version=7.0.0b0
+lab_manager_version=0.24.7
 
-conda create -n $env python=3.6 numpy -y
+conda create -n $env python=${python_version} numpy nomkl -y
 source activate $env
 
-conda install notebook==5.0.0 -c conda-forge
-pip install ipywidgets==7.0.0b0
+conda install notebook==${notebook_version} -c conda-forge
+pip install ipywidgets==${ipywidgets_version}
+pip install widgetsnbextension==3.0.0b2 --upgrade
+jupyter nbextension install --py --sys-prefix widgetsnbextension
 jupyter nbextension enable --py --sys-prefix widgetsnbextension
 
-conda install jupyterlab==0.25.2 -c conda-forge -y
-jupyter labextension install @jupyter-widgets/jupyterlab-manager # 0.24.7
+conda install jupyterlab==${jupyterlab_version} -c conda-forge -y
+jupyter labextension install @jupyter-widgets/jupyterlab-manager@${lab_manager_version}
 
 cd $nglview_src
 
