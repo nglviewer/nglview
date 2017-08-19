e8f26cfaff43725e9a88a3b87548edadfccd6400
Hai Nguyen <hainm@users.noreply.github.com>
2017-08-19 03:18:21 -0400

    Officially support ipywigets 7 (#682)
    * Officially support ipywigets 7
    
    * pip

--- devtools/nglview-jupyterlab.sh
+++ devtools/nglview-jupyterlab.sh
@@ -6,8 +6,8 @@ jupyterlab_version=0.26.5
 python_version=3.6
 notebook_version=5.0.0
 lab_manager_version=0.25.11 # need to match to ./jslab/package.json
-ipywidgets_version=7.0.0b12
-widgetsnbextension_version=3.0.0b12
+ipywidgets_version=7.0.0
+widgetsnbextension_version=3.0.0
 
 echo "nglview root folder: $nglview_src"
 echo "Creating env $env"
@@ -17,7 +17,7 @@ source activate $env
 conda install setuptools -c conda-forge --force -y
 conda install notebook==${notebook_version} -c conda-forge -y
 pip install ipywidgets==${ipywidgets_version}
-pip install widgetsnbextension==${widgetsnbextension} --upgrade
+pip install widgetsnbextension==${widgetsnbextension_version} --upgrade
 jupyter nbextension install --py --sys-prefix widgetsnbextension
 jupyter nbextension enable --py --sys-prefix widgetsnbextension
 
