f9655a9e5ba5f302e69d02e6ed34109524d0bbbb
HN <nhai.qn@gmail.com>
2017-10-09 00:46:26 -0400

    fix version

--- devtools/nglview-jupyterlab.sh
+++ devtools/nglview-jupyterlab.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 nglview_src=`pwd`
-env=jupyterlab_0265
-jupyterlab_version=0.26.5
+env=jupyterlab_0270
+jupyterlab_version=0.27.0
 python_version=3.6
 notebook_version=5.0.0
 lab_manager_version=0.27.0 # need to match to ./jslab/package.json
