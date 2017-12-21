7554eece38989e41fbc6d84efa54f8e6d94f808d
HN <nhai.qn@gmail.com>
2017-12-21 11:41:03 +0700

    update nglview-js-widgets version

--- nglview/widget.py
+++ nglview/widget.py
@@ -28,7 +28,7 @@ from .config import BACKENDS
 from .remote_thread import RemoteCallThread
 
 __all__ = ['NGLWidget', 'ComponentViewer']
-__frontend_version__ = '0.5.4-dev.30' # must match to js/package.json and js/src/widget_ngl.js
+__frontend_version__ = '0.5.4-dev.31' # must match to js/package.json and js/src/widget_ngl.js
 _EXCLUDED_CALLBACK_AFTER_FIRING = {
         'setUnSyncCamera', 'setSelector', 'setUnSyncFrame', 'setDelay',
         'autoView',
