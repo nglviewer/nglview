9878788b3a722226906c63633bf1dea1d1690b08
HN <nhai.qn@gmail.com>
2018-03-18 18:25:06 -0400

    nglview 1.1

--- devtools/nglview-jupyterlab.sh
+++ devtools/nglview-jupyterlab.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 nglview_src=`pwd`
-env=jupyterlab_0270
-jupyterlab_version=0.27.0
+env=jupyterlab_0_31
+jupyterlab_version=0.31
 lab_manager_version=${jupyterlab_version} # need to match to ./jslab/package.json too too.
 python_version=3.6
 notebook_version=5.0.0
-ipywidgets_version=7.0.0
-widgetsnbextension_version=3.0.0
+ipywidgets_version=7.1.2
+# widgetsnbextension_version=3.0.0
 
 echo "nglview root folder: $nglview_src"
 echo "Creating env $env"
@@ -17,7 +17,7 @@ source activate $env
 conda install setuptools -c conda-forge --force -y
 conda install notebook==${notebook_version} -c conda-forge -y
 pip install ipywidgets==${ipywidgets_version}
-pip install widgetsnbextension==${widgetsnbextension_version} --upgrade
+# pip install widgetsnbextension==${widgetsnbextension_version} --upgrade
 pip install pytraj # comment if using windows
 jupyter nbextension install --py --sys-prefix widgetsnbextension
 jupyter nbextension enable --py --sys-prefix widgetsnbextension
