546940598c239a36afe27a49ae3d84bf96fcdf44
HN <nhai.qn@gmail.com>
2018-03-23 23:04:49 -0400

    correct recipe for nglview-jupyterlab.sh

--- devtools/nglview-jupyterlab.sh
+++ devtools/nglview-jupyterlab.sh
@@ -1,8 +1,8 @@
 conda create -n lab python=3.6 -y
 source activate lab
-conda install jupyterlab=0.31.12 -y -c conda-force
+conda install ipywidgets=7.1.2 -c conda-forge -y
 pip install nglview==1.1.2
 nglview enable # might need this to enable nglview-js-widgets extension for notebook
-conda install ipywidgets=7.1.2 -c conda-forge -y
+conda install jupyterlab=0.31.12 -y -c conda-forge
 jupyter-labextension install @jupyter-widgets/jupyterlab-manager@0.33.2
 jupyter-labextension install nglview-js-widgets@1.1.2
