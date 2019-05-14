9d709111f6be1292050bfbf33ee2d59f01d63af3
Hai Nguyen <hainm.comp@gmail.com>
2019-05-14 12:13:43 -0400

    ADD devtools/release.py

---
+++ devtools/release.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import subprocess
+import sys
+import nglview
+
+print(nglview)
+
+front_end_version = nglview.widget.__frontend_version__
+latest_tag = sorted(subprocess.check_output('git tag', shell=True).decode().split('\n'))[-1]
+latest_tag = latest_tag.replace('v', '')
+
+if front_end_version != latest_tag:
+    print(f"Version mismatch between front_end_version {front_end_version} and latest_tag {latest_tag}")
+    sys.exit(1)
+subprocess.check_call('python setup.py sdist', shell=True)
