75e444fac584d351e2271f491d29ed1b45ce1f6b
HN <nhai.qn@gmail.com>
2019-08-04 13:51:13 -0400

    more

--- js/src/base.js
+++ js/src/base.js
@@ -1,34 +1,35 @@
-var widgets = require("@jupyter-widgets/base")
-var NGL = require("ngl")
-
-
-var BaseView = widgets.DOMWidgetView.extend({
-
-    render: function(){
-        this.handleMessage();
-        this.displayed.then(() =>{
-            this.model.set("_ready", true)
-            this.touch()
-        })
-    },
-
-    executeCode: function(code){
-        eval(code);
-    },
-
-    handleMessage: function(){
-        this.model.on("msg:custom", function(msg){
-            this.on_msg(msg)
-        }.bind(this))
-    },
-
-    on_msg: function(msg){
-        if (msg.type == 'callMethod'){
-            this[msg.methodName].apply(this, msg.args, msg.kwargs)
-        }
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var widgets = require("@jupyter-widgets/base");
+var BaseView = (function (_super) {
+    __extends(BaseView, _super);
+    function BaseView() {
+        _super.apply(this, arguments);
     }
-})
-
-module.exports = {
-    "BaseView": BaseView
-}
+    BaseView.prototype.render = function () {
+        var _this = this;
+        this.handleMessage();
+        this.displayed.then(function () {
+            _this.model.set("_ready", true);
+            _this.touch();
+        });
+    };
+    BaseView.prototype.executeCode = function (code) {
+        eval(code);
+    };
+    BaseView.prototype.handleMessage = function () {
+        this.model.on("msg:custom", function (msg) {
+            this.on_msg(msg);
+        }.bind(this));
+    };
+    BaseView.prototype.on_msg = function (msg) {
+        if (msg.type == 'callMethod') {
+            this[msg.methodName].apply(this, msg.args, msg.kwargs);
+        }
+    };
+    return BaseView;
+})(widgets.DOMWidgetView);
+exports.BaseView = BaseView;
--- js/src/widget_ngl.ts
+++ js/src/widget_ngl.ts
@@ -18,6 +18,7 @@ import 'jquery-ui/themes/base/all.css'
 // require('./css/dark.css');  // How to switch theme?
 import '../src/css/light.css'
 import '../src/css/main.css'
+import { FullscreenModel, FullscreenView } from "./fullscreen"
 
 
 // From NGL
@@ -1112,61 +1113,6 @@ class NGLView extends widgets.DOMWidgetView{
             }
         }
     }
-}
-
-export
-class FullscreenModel extends widgets.DOMWidgetModel{
-    defaults(){
-        return _.extend(widgets.DOMWidgetModel.prototype.defaults(), {
-            _model_name: 'FullscreenModel',
-            _model_module: 'nglview-js-widgets',
-            _model_module_version: require("../package.json").version,
-            _view_name: "FullscreenView",
-            _view_module: "nglview-js-widgets",
-            _view_module_version: require("../package.json").version,
-        });
-    }
-}
-
-export
-class FullscreenView extends BaseView{
-    render() {
-        this.stage = new NGL.Stage()
-        var that = this
-        this.model.on("msg:custom", function(msg){
-            that.on_msg(msg)
-        })
-        this.handleSignals()
-    }
-
-    fullscreen(model_id){
-        var that = this
-        this.model.widget_manager.get_model(model_id).then((model) =>{
-            var key = Object.keys(model.views)[0]
-            model.views[key].then((view) => {
-                that.stage.toggleFullscreen(view.el)
-            })
-        })
-    }
-
-    handleSignals(){
-        var that = this
-        this.stage.signals.fullscreenChanged.add(function (isFullscreen) {
-            that.model.set("_is_fullscreen", isFullscreen)
-            that.touch()
-        })
-    }
-
-    executeCode(code){
-        eval(code);
-    }
-
-    on_msg(msg){
-        if ('executeCode' in msg){
-            this.executeCode(msg.executeCode)
-        }
-    }
-
 }
 
 // export all models and views here to make embeding a bit easier
