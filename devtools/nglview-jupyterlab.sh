e5a5bd031298121ae16bbbfd7b5197548ce794f9
Hai Nguyen <hainm@users.noreply.github.com>
2020-06-23 22:18:42 -0400

    jupyterlab (#909)

--- devtools/nglview-jupyterlab.sh
+++ devtools/nglview-jupyterlab.sh
@@ -1,14 +1,12 @@
-pythonversion=3.7
-nglviewversion=1.1.7
-labversion=0.34.12
-labmanagerversion=0.37.4
-ipywidgetsversion=7.4.2
+pythonversion=3.8
+nglviewversion=2.7.7
+labversion=2.1.5
+ipywidgetsversion=7.5.1
 
 conda create -n lab python=$pythonversion -y
 source activate lab
-conda install ipywidgets=$ipywidgetsversion -c conda-forge -y
-pip install nglview==$nglviewversion
-nglview enable # might need this to enable nglview-js-widgets extension for notebook
+conda install ipywidgets==$ipywidgetsversion -c conda-forge -y
+conda install nglview==$nglviewversion -c conda-forge -y
 conda install jupyterlab=$labversion  -y -c conda-forge
-jupyter-labextension install @jupyter-widgets/jupyterlab-manager@$labmanagerversion
+jupyter-labextension install @jupyter-widgets/jupyterlab-manager
 jupyter-labextension install nglview-js-widgets@$nglviewversion
