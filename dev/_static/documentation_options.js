453a7f89de812e6c6744e0a5781834f742fb69e7
HN <nhai.qn@gmail.com>
2020-12-22 23:41:15 -0500

    dev

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.7.7+12.g5136ac2.dirty',
+    VERSION: '2.7.7+13.gd28e3c5',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
