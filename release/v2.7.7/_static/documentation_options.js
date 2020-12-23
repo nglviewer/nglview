039d32febbd87c45c9ffb2421123aec3d5953341
HN <nhai.qn@gmail.com>
2020-12-22 23:36:16 -0500

    update

--- release/v2.7.7/_static/documentation_options.js
+++ release/v2.7.7/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.7.7+1.gf915b3e.dirty',
+    VERSION: '2.7.7+0.gd41dfb9.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
