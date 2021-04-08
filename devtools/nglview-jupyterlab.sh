11549d38ea4f014056c80c620afc38803c9acc2e
Hai Nguyen <hainm@users.noreply.github.com>
2021-04-08 02:00:07 -0400

    Update nglview-jupyterlab.sh

--- devtools/nglview-jupyterlab.sh
+++ devtools/nglview-jupyterlab.sh
@@ -1,6 +1,3 @@
 conda create -n lab python=3.8 -y 
-source  activate lab
-conda install jupyterlab=3 -c conda-forge -y
-pip install jupyterlab_widgets
-pip install nglview # Python part
-pip install https://github.com/nglviewer/nglview/releases/download/v2.7.7/nglview-js-widgets-2.7.7.tar.gz # JS part for Lab
+source activate lab
+conda install jupyterlab=3 nglview -c conda-forge -y
