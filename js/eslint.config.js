714570b20744ae8c9e5eea6307373256ee1d4f38
Hai Nguyen <hainm.comp@gmail.com>
2024-06-19 18:31:56 -0400

    remove

--- js/eslint.config.js
+++ js/eslint.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  extends: 'eslint:recommended',
+  files: [
+      "**/*.ts",
+      "**/*.cts",
+      "**.*.mts"
+  ],
   rules: {
     'no-console': 'off',
     'no-constant-condition': 'off',
@@ -8,15 +11,6 @@ module.exports = {
     'no-redeclare': 'off',
     'valid-jsdoc': 'error',
   },
-  env: {
-    es6: true,
-    node: true,
-    mocha: true,
-    browser: true,
-    worker: true,
-  },
-  parserOptions: {
-    sourceType: 'module',
-  },
-  ignorePatterns: ['node_modules', 'dist', 'coverage', '**/*.d.ts'],
+  ignorePatterns: ['node_modules', 'dist', 'coverage']
+
 };
