5c30ee47bd8b9e02c2e0182837a08a446e89d9bc
Hai Nguyen <hainm.comp@gmail.com>
2024-06-19 18:26:12 -0400

    eslint.config.js

---
+++ js/eslint.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: 'eslint:recommended',
+  rules: {
+    'no-console': 'off',
+    'no-constant-condition': 'off',
+    'no-unused-vars': 'off',
+    'no-redeclare': 'off',
+    'valid-jsdoc': 'error',
+  },
+  env: {
+    es6: true,
+    node: true,
+    mocha: true,
+    browser: true,
+    worker: true,
+  },
+  parserOptions: {
+    sourceType: 'module',
+  },
+  ignorePatterns: ['node_modules', 'dist', 'coverage', '**/*.d.ts', 'tests'],
+};
