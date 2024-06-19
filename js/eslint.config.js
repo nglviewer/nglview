40d4cf94e86f08b25e70379e7ba4e4d4e6f17ea7
Hai Nguyen <hainm.comp@gmail.com>
2024-06-19 19:29:31 -0400

    more

--- js/eslint.config.js
+++ js/eslint.config.js
@@ -1,3 +1,15 @@
 module.exports = {
     files: ["**/*.ts"],
+     parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  extends: [
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+  ],
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+  },
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+  },
 };
