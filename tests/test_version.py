d3b099ad338c4006880e5bd5a80b88fe1efb32fa
Hai Nguyen <hainm@users.noreply.github.com>
2024-11-25 00:35:15 -0500

    Move tests (#1135)

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,15 +1,15 @@
 import json
-import os
+from pathlib import Path
 
 import nglview as nv
 
 
 def test_make_sure_versions_matched():
-    nglview_dir = os.path.join(os.path.dirname(__file__), '..')
+    nglview_dir = Path(__file__).resolve().parent.parent / 'nglview'
 
     # frontend and backend
-    nglview_js_widgets = os.path.join(nglview_dir, 'static', 'index.js')
-    with open(nglview_js_widgets) as fh:
+    nglview_js_widgets = nglview_dir / 'static' / 'index.js'
+    with nglview_js_widgets.open() as fh:
         for line in fh:
             if '"name":"nglview-js-widgets"' in line:
                 break
