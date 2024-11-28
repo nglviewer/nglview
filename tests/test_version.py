fb73b1d71e69f862961931f0e23a6218fa41ed9a
Hai Nguyen <hainm@users.noreply.github.com>
2024-11-27 20:44:39 -0500

    Ngl.2.4.0 (#1137)

--- tests/test_version.py
+++ tests/test_version.py
@@ -8,10 +8,5 @@ def test_make_sure_versions_matched():
     nglview_dir = Path(__file__).resolve().parent.parent / 'nglview'
 
     # frontend and backend
-    nglview_js_widgets = nglview_dir / 'static' / 'index.js'
-    with nglview_js_widgets.open() as fh:
-        for line in fh:
-            if '"name":"nglview-js-widgets"' in line:
-                break
-    line = line.replace('module.exports =', '')
-    assert json.loads(line)['version'] == nv.widget.__frontend_version__
+    json_str = (nglview_dir / "staticlab/package.json").read_text()
+    assert json.loads(json_str)['version'] == nv.widget.__frontend_version__
