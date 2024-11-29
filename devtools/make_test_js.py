4d190433717edd7013d8ba5528007b5fe6389674
Hai Nguyen <hainm.comp@gmail.com>
2024-11-29 15:20:00 -0500

    yapf

--- devtools/make_test_js.py
+++ devtools/make_test_js.py
@@ -5,6 +5,7 @@ import os
 from glob import glob
 from pathlib import Path
 
+
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('--basic', action='store_true')
@@ -13,6 +14,7 @@ def parse_arguments():
     parser.add_argument('-n', '--nb', nargs='?')
     return parser.parse_args()
 
+
 def get_notebooks(args):
     api_root_dir = 'tests/notebooks/api/'
     if args.api:
@@ -20,7 +22,9 @@ def get_notebooks(args):
     elif args.single:
         return [args.nb]
     else:
-        return glob('tests/notebooks/test*ipynb') + glob('tests/notebooks/api/test*ipynb')
+        return glob('tests/notebooks/test*ipynb') + glob(
+            'tests/notebooks/api/test*ipynb')
+
 
 def get_cell_length(nb):
     n_cells = 0
@@ -29,6 +33,7 @@ def get_cell_length(nb):
             if 'cell_type' in line:
                 n_cells += 1
     return n_cells
+
 
 def generate_js(notebooks_with_cell_lengths):
     head = """
@@ -49,9 +54,9 @@ module.exports = {
     tail = """
 }
 """
-    all_notebooks = '\n'.join(
-        body_template % (notebook, notebook, n_cells)
-        for (notebook, n_cells) in notebooks_with_cell_lengths)
+    all_notebooks = '\n'.join(body_template % (notebook, notebook, n_cells)
+                              for (notebook,
+                                   n_cells) in notebooks_with_cell_lengths)
 
     js_dir = Path('tests/js')
     js_dir.mkdir(parents=True, exist_ok=True)
@@ -59,8 +64,11 @@ module.exports = {
     fn = js_dir / 'test.js'
     fn.write_text(head + all_notebooks + tail)
 
+
 if __name__ == '__main__':
     args = parse_arguments()
     notebooks = get_notebooks(args)
-    notebooks_with_cell_lengths = [(nb, 2 * get_cell_length(nb)) for nb in notebooks]
+    notebooks_with_cell_lengths = [
+        (nb, 2 * get_cell_length(nb)) for nb in notebooks
+    ]
     generate_js(notebooks_with_cell_lengths)
