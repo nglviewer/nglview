7bc5fb6263d084e329884c9edffb33d23a57456e
Hai Nguyen <hainm.comp@gmail.com>
2024-11-29 15:18:17 -0500

    test

--- devtools/make_test_js.py
+++ devtools/make_test_js.py
@@ -10,55 +10,41 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--basic', action='store_true')
 parser.add_argument('--api', action='store_true')
 parser.add_argument('--single', action='store_true')
-parser.add_argument('--travis', action='store_true')
 parser.add_argument('-n', '--nb', nargs='?')
 args = parser.parse_args()
 
 api_root_dir = 'tests/notebooks/api/'
 
-notebook_names_for_travis  = [
-             'test_no_gui_demo.ipynb',
-             'test_add_structure_then_trajectory.ipynb',
-             'test_automatically_added_attributes_0.ipynb',
-            ]
-
-if args.travis:
-    notebook_names = notebook_names_for_travis
-    notebooks = [api_root_dir + notebook_name for notebook_name in notebook_names]
-elif args.api:
-    notebooks = glob(api_root_dir + '/test*.ipynb')
+if args.api:
+    notebooks = glob(os.path.join(api_root_dir, 'test*.ipynb'))
 elif args.single:
     notebooks = [args.nb]
 else:
-    notebooks = ['tests/notebooks/dummy.ipynb',]
-    
+    notebooks = []
     notebooks += (glob('tests/notebooks/test*ipynb') +
-                glob('tests/notebooks/api/test*ipynb'))
+                  glob('tests/notebooks/api/test*ipynb'))
 
 # shuffle(notebooks)
 def get_cell_length(nb):
     n_cells = 0
-
     with open(nb) as fh:
-        for line in fh.readlines():
+        for line in fh:
             if 'cell_type' in line:
                 n_cells += 1
     return n_cells
 
-notebooks_with_cell_lengths = [(nb, 2*get_cell_length(nb)) for nb in notebooks]
+notebooks_with_cell_lengths = [(nb, 2 * get_cell_length(nb)) for nb in notebooks]
 
 head = """
 module.exports = {
-
 """
-
 
 body_template = """
     "%s": function (browser) {
         browser.openNotebook("%s");
         browser.restartKernel(2000);
-        for ( var i = 0; i < %s; i++) {
-           browser.executeCell(i)
+        for (var i = 0; i < %s; i++) {
+            browser.executeCell(i)
                   .pause(3000)
                   .cellHasError(i);
         }
@@ -71,18 +57,13 @@ tail = """
 """
 
 if __name__ == '__main__':
+    from pathlib import Path
 
     all_notebooks = '\n'.join(body_template % (notebook, notebook, n_cells)
                               for (notebook, n_cells) in notebooks_with_cell_lengths)
 
-    fn = 'tests/js/test.js'
-    with open(fn, 'w') as fh:
-        fh.write(head + all_notebooks + tail)
+    js_dir = Path('tests/js')
+    js_dir.mkdir(parents=True, exist_ok=True)
 
-    for nb_abspath, n_cells in notebooks_with_cell_lengths:
-        basename = os.path.basename(nb_abspath)
-        js_fn = os.path.join('tests/js',
-                os.path.splitext(basename)[0] + '.js')
-        with open(js_fn, 'w') as fh:
-            body = body_template % (nb_abspath, nb_abspath, n_cells)
-            fh.write(head + body + tail)
+    fn = js_dir / 'test.js'
+    fn.write_text(head + all_notebooks + tail)
