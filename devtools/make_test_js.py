fb5a47880c0ffbfd3e009c62eba134371b197783
Hai Nguyen <hainm.comp@gmail.com>
2024-11-29 15:19:36 -0500

    refactor

--- devtools/make_test_js.py
+++ devtools/make_test_js.py
@@ -2,29 +2,26 @@
 
 import argparse
 import os
-import subprocess
 from glob import glob
-from random import shuffle
+from pathlib import Path
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--basic', action='store_true')
-parser.add_argument('--api', action='store_true')
-parser.add_argument('--single', action='store_true')
-parser.add_argument('-n', '--nb', nargs='?')
-args = parser.parse_args()
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--basic', action='store_true')
+    parser.add_argument('--api', action='store_true')
+    parser.add_argument('--single', action='store_true')
+    parser.add_argument('-n', '--nb', nargs='?')
+    return parser.parse_args()
 
-api_root_dir = 'tests/notebooks/api/'
+def get_notebooks(args):
+    api_root_dir = 'tests/notebooks/api/'
+    if args.api:
+        return glob(os.path.join(api_root_dir, 'test*.ipynb'))
+    elif args.single:
+        return [args.nb]
+    else:
+        return glob('tests/notebooks/test*ipynb') + glob('tests/notebooks/api/test*ipynb')
 
-if args.api:
-    notebooks = glob(os.path.join(api_root_dir, 'test*.ipynb'))
-elif args.single:
-    notebooks = [args.nb]
-else:
-    notebooks = []
-    notebooks += (glob('tests/notebooks/test*ipynb') +
-                  glob('tests/notebooks/api/test*ipynb'))
-
-# shuffle(notebooks)
 def get_cell_length(nb):
     n_cells = 0
     with open(nb) as fh:
@@ -33,13 +30,11 @@ def get_cell_length(nb):
                 n_cells += 1
     return n_cells
 
-notebooks_with_cell_lengths = [(nb, 2 * get_cell_length(nb)) for nb in notebooks]
-
-head = """
+def generate_js(notebooks_with_cell_lengths):
+    head = """
 module.exports = {
 """
-
-body_template = """
+    body_template = """
     "%s": function (browser) {
         browser.openNotebook("%s");
         browser.restartKernel(2000);
@@ -51,19 +46,21 @@ body_template = """
         browser.end();
     },
 """
-
-tail = """
+    tail = """
 }
 """
-
-if __name__ == '__main__':
-    from pathlib import Path
-
-    all_notebooks = '\n'.join(body_template % (notebook, notebook, n_cells)
-                              for (notebook, n_cells) in notebooks_with_cell_lengths)
+    all_notebooks = '\n'.join(
+        body_template % (notebook, notebook, n_cells)
+        for (notebook, n_cells) in notebooks_with_cell_lengths)
 
     js_dir = Path('tests/js')
     js_dir.mkdir(parents=True, exist_ok=True)
 
     fn = js_dir / 'test.js'
     fn.write_text(head + all_notebooks + tail)
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    notebooks = get_notebooks(args)
+    notebooks_with_cell_lengths = [(nb, 2 * get_cell_length(nb)) for nb in notebooks]
+    generate_js(notebooks_with_cell_lengths)
